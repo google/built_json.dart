6ac2cdd347a6c3364f796950dd2646f8e185e175
David Morgan <davidmorgan@google.com>
2016-01-23 10:43:55 +0100

    Update source_library.dart

--- built_json_generator/lib/src/source_library.dart
+++ built_json_generator/lib/src/source_library.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2015, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 library built_json_generator.source_library;
 
 import 'package:analyzer/src/generated/element.dart';
